d357421f212ee27642daca459f09aa8e4a03ddb1
yemur <2085151308@qq.com>
2026-01-21 15:05:25 +0800

    构建更新

--- komari-web/src/components/Footer.tsx
+++ komari-web/src/components/Footer.tsx
@@ -1,41 +1,6 @@
-import { Flex, Text } from '@radix-ui/themes';
-import { useEffect, useState } from 'react';
-import { useRPC2Call } from '@/contexts/RPC2Context';
+import { Flex } from '@radix-ui/themes';
 
 const Footer = () => {
-  //const currentYear = new Date().getFullYear();
-  
-  // 格式化 build 时间
-  const formatBuildTime = (isoString: string) => {
-    const date = new Date(isoString);
-    return date.toLocaleString('zh-CN', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      timeZone: 'Asia/Shanghai'
-    }) + ' (GMT+8)';
-  };
-
-  const buildTime = typeof __BUILD_TIME__ !== 'undefined' ? __BUILD_TIME__ : null;
-  const [versionInfo, setVersionInfo] = useState<{ hash: string; version: string } | null>(null);
-  const { call } = useRPC2Call();
-
-  useEffect(() => {
-    const fetchVersionInfo = async () => {
-      try {
-        //const response = await fetch('/api/version');
-  const data = await call("common:getVersion")
-          setVersionInfo({ hash: data.hash?.slice(0,7), version: data.version });
-      } catch (error) {
-        console.error('Failed to fetch version info:', error);
-      }
-    };
-
-    fetchVersionInfo();
-  }, []);
   return (
     <div
       className='footer p-2 border-t-1 border-t-[var(--gray-7)]'
